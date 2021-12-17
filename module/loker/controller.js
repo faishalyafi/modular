34005591e4a50fa45c0c5c2e915b0b53f6c167eb
ika776 <ytian3280@gmail.com>
2021-12-17 16:28:12 +0700

    loker

--- module/loker/controller.js
+++ module/loker/controller.js
@@ -1,6 +1,6 @@
-const loker = require("../model/lokerModel");
+const loker = require("./model");
 const { v4: uuid_v4 } = require("uuid");
-const sq = require("../config/connection");
+const sq = require("../../config/connection");
 
 class Controller {
   static register(req, res) {
--- module/loker/route.js
+++ module/loker/route.js
@@ -1,6 +1,6 @@
-const Controller = require('../controller/lokerController');
+const Controller = require('./controller');
 const router = require('express').Router();
-const authentification = require('../middleware/authentification');
+const authentification = require('../../middleware/authentification');
 
 router.post('/register', authentification, Controller.register);
 router.get('/list', authentification, Controller.list);
