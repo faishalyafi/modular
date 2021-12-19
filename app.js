7b2bf2ee1d23e2e41d60086192779856b7076257
faishalyafi <faishalyafiee@gmail.com>
2021-12-19 13:32:25 +0700

    PR

--- app.js
+++ app.js
@@ -9,18 +9,13 @@ const routing = require('./routing/index')
 
 app.use(morgan('dev'))
 app.use(cors())
-app.use(express.urlencoded({
-	extended: false
-}))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 // app.use(express.static('asset/file'));
 // app.use(express.static('asset/pdf'));
 app.use( '/asset/file', express.static( 'asset/file' ) );
 app.use( '/asset/pdf', express.static( 'asset/pdf' ) );
 
-app.set("view engine", "ejs");
-app.set('views', __dirname + '/views');
-// app.set("views", path.join(__dirname, "views"));
 
 app.use('/', routing)
 
