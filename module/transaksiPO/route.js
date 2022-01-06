ec922a655807aeb7849f41c70b8c1e8a55f1be00
budi-irawan <budi.irawan.171717@gmail.com>
2022-01-06 16:38:21 +0700

    transaksiPO route

--- module/transaksiPO/route.js
+++ module/transaksiPO/route.js
@@ -7,6 +7,7 @@ router.post("/update", authentification, Controller.update);
 router.post("/delete", authentification, Controller.delete);
 router.get("/detailsById/:id", authentification, Controller.detailsById);
 router.get("/detailsTransaksiByNomorPO/:nomorPO",authentification,Controller.detailsTransaksiByNomorPO);
+router.get("/listTransaksiPObyNomerPO/:nomorPO",authentification,Controller.listTransaksiPObyNomerPO);
 router.get("/historyAllTransaksi",authentification,Controller.historyAllTransaksi);
 router.get("/listAllTransaksi", authentification, Controller.listAllTransaksi);
 // router.post("/updateBarang", authentification, Controller.updateBarang);
